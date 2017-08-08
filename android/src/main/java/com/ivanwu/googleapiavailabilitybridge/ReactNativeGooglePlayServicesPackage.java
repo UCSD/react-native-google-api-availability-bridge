280c82c051519f5670143b637c768896c9aae2ab
satishmane <satish.mane@aeris.net>
2017-08-08 18:41:24 +0530

    RN0.47Fix

--- android/src/main/java/com/ivanwu/googleapiavailabilitybridge/ReactNativeGooglePlayServicesPackage.java
+++ android/src/main/java/com/ivanwu/googleapiavailabilitybridge/ReactNativeGooglePlayServicesPackage.java
@@ -21,6 +21,6 @@ public class ReactNativeGooglePlayServicesPackage implements ReactPackage {
     }
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Collections.emptyList();
+        return Arrays.<ViewManager>asList();
     }
 }
